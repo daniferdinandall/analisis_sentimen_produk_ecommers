1f6285e38e9dd914e573c1c89305fc52ab8ca55c
aulianafahrian <aulianafahrian@gmail.com>
2024-07-03 10:56:09 +0700

    feat: Add scraping functionality for Tokopedia website

--- Scraping/ScrapingTokopedia/scrape.py
+++ Scraping/ScrapingTokopedia/scrape.py
@@ -1,37 +1,57 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 import pandas as pd
 
+# Meminta pengguna untuk memasukkan URL toko
 url = input("Masukkan url toko : ")
 
-if url :
+if url:
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=options)
     driver.get(url)
 
     data = []
-    for i in range(0, 28):
+
+    # Definisikan fungsi untuk klik "Selengkapnya"
+    def klik_selengkapnya():
+        buttons = driver.find_elements(By.CSS_SELECTOR, "button.css-89c2tx")
+        for button in buttons:
+            try:
+                button.click()  # Klik tombol 'Selengkapnya'
+                time.sleep(1)  # Tunggu 1 detik untuk memuat ulasan tambahan
+            except Exception as e:
+                print(f"Error saat mengklik 'Selengkapnya': {str(e)}")
+
+    # Panggil fungsi untuk klik "Selengkapnya" setelah membuka halaman
+    klik_selengkapnya()
+
+    for i in range(0, 31):
         soup = BeautifulSoup(driver.page_source, "html.parser")
-        containers = soup.findAll('article', attrs = {'class':'css-72zbc4'})
+        containers = soup.findAll('article', attrs={'class': 'css-72zbc4'})
 
         for container in containers:
             try:
-                review = container.find('span', attrs = {'data-testid':'lblItemUlasan'}).text
-                data.append(
-                    (review)
-                )
+                review = container.find(
+                    'span', attrs={'data-testid': 'lblItemUlasan'}).text.strip()
+                data.append((review))
             except AttributeError:
                 continue
 
         time.sleep(2)
-        driver.find_element(By.CSS_SELECTOR, "button[aria-label^='Laman berikutnya']").click()
-        time.sleep(3)
+        try:
+            driver.find_element(
+                By.CSS_SELECTOR, "button[aria-label^='Laman berikutnya']").click()
+            time.sleep(3)
+            klik_selengkapnya()  # Panggil fungsi untuk klik "Selengkapnya" di halaman berikutnya
+        except Exception as e:
+            print(f"Error saat mengklik halaman berikutnya: {str(e)}")
+            break
 
     print(data)
     df = pd.DataFrame(data, columns=["Ulasan"])
-    df.to_csv("Aerostreet3.csv", index=False)
+    df.to_csv("Aerostreet4.csv", index=False)
+
+    driver.quit()  # Menutup browser Selenium setelah selesai
