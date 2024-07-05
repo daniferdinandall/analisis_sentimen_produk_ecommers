d578350cd2cb26c1de8adc4a1ce406a3e215921a
dani ferdinan <daniferdinandall@gmail.com>
2024-07-05 15:07:11 +0700

    Update CSV file name for scraped data

--- Scraping/ScrapingTokopedia/scrapenegative.py
+++ Scraping/ScrapingTokopedia/scrapenegative.py
@@ -15,7 +15,30 @@ if url:
 
     data = []
 
+   # Temukan semua elemen label yang mengandung checkbox dan rating
+    labels = driver.find_elements(By.CSS_SELECTOR, "label.checkbox")
+
+    for label in labels:
+            try:
+                # Temukan elemen yang mengandung rating
+                rating_element = label.find_element(
+                    By.CSS_SELECTOR, "p.rating-star span")
+                rating = rating_element.text
+
+                # Klik checkbox jika ratingnya 4, 3, 2, atau 1
+                if rating in ["4", "3", "2", "1"]:
+                    checkbox = label.find_element(
+                        By.CSS_SELECTOR, "div[data-testid='ratingFilter']")
+                    checkbox.click()
+                    time.sleep(1)  # Tambahkan jeda waktu jika diperlukan
+            except Exception as inner_e:
+                print(
+                    f"Error saat mencoba mengklik rating {rating}: {str(inner_e)}")
+
+    time.sleep(2)  # Tunggu sejenak setelah klik
+
     # Definisikan fungsi untuk klik "Selengkapnya"
+
     def klik_selengkapnya():
         buttons = driver.find_elements(By.CSS_SELECTOR, "button.css-89c2tx")
         for button in buttons:
@@ -52,6 +75,6 @@ if url:
 
     print(data)
     df = pd.DataFrame(data, columns=["Ulasan"])
-    df.to_csv("Aerostreet7.csv", index=False)
+    df.to_csv("Aerostreetneg1.csv", index=False)
 
     driver.quit()  # Menutup browser Selenium setelah selesai
